7dfec0b5a29eb7f4470a4f8fc9dee667bd3dcfa0
Abdellah <abdellahkaram19@gmail.com>
2023-03-22 16:18:45 -0700

    third commit  in c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -30,8 +30,8 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) 10 + 48); /*git the first digit*/
-			_putchar((prod % 10) 10 + 48); /*git the second digit*/
+				_putchar((prod / 10) + 48); /*git the first digit*/
+			_putchar((prod % 10) + 48); /*git the second digit*/
 		}
 		_putchar('\n');
 	}
