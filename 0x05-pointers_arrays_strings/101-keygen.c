01c3aeeaded82a0b87a0f8552c2476478274bfb6
Abdellah <abdellahkaram19@gmail.com>
2023-03-27 19:33:54 -0700

    a

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Generates random valid passwords for the
+ *        program 101-crackme.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	return (0);
+}
